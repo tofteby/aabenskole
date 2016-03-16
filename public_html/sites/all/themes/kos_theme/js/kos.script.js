cebf528f4d7ee5eaf2223af26956270fedbcdf90
Mathias Larsen <ml@bellcom.dk>
2016-03-16 10:44:20 +0100

    Fix equalheights

--- public_html/sites/all/themes/kos_theme/js/kos.script.js
+++ public_html/sites/all/themes/kos_theme/js/kos.script.js
@@ -7,27 +7,28 @@
 (function ($, Drupal) {
 'use strict';
   var basepath;
-  
+
   Drupal.behaviors.kosHeights = {
     attach: function (context, settings) {
         // Hack to have a minimum height on pages.
         function fixBodyHeight() {
-            $(".sec.sec-content").css('padding-bottom',0);
-            var delta = $(window).height() - $('body').height();
-            if(delta>0){
-                $(".sec.sec-content").css('padding-bottom',delta);
-            }
+          $(".sec.sec-content").css('padding-bottom',0);
+          var delta = $(window).height() - $('body').height();
+          if(delta>0){
+            $(".sec.sec-content").css('padding-bottom',delta);
+          }
         }
 
         // Equal heights on news rows.
-        $(".view-school-events-list", context).each(function(){
+        $(window).load(function() {
+          $(".view-school-events-list", context).each(function() {
             $(".views-row", $(this)).equalHeights();
-            
-         });
-       
+          });
+        });
+
         fixBodyHeight();
         $(window).resize(function() {
-            fixBodyHeight();
+          fixBodyHeight();
         });
     }
   };
